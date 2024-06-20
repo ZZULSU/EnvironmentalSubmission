e13d0392aaaec092f2bc05a6e56cef1a96c1c237
Hyunseok Son <archerson96@gmail.com>
2024-06-20 17:06:58 +0200

    check

--- assignment.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-
-# Load your data into a pandas DataFrame
-data = pd.read_csv('yield_piecewise_X.csv')
